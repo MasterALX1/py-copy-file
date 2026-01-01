104fca14a7f00f797b9d515ece3383dfb0d56771
Alexandre Schmid <alex@alxcloudit.com>
2026-01-01 15:11:37 -0500

    Solution corrected based on comments

--- app/main.py
+++ app/main.py
@@ -2,14 +2,19 @@ def copy_file(command: str) -> None:
     if not command.startswith("cp "):
         return
     try:
-        source_file = command.split(" ")[1]
-        destination_file = command.split(" ")[2]
-        if source_file == destination_file:
+        command_parts = command.split(" ")
+        if (
+            len(command_parts) != 3
+            or not command_parts[0] == "cp"
+            or command_parts[1] == command_parts[2]
+        ):
             return
+
         with (
-            open(source_file, "r") as file_in,
-            open(destination_file, "w") as file_out,
+            open(command_parts[1], "r") as file_in,
+            open(command_parts[2], "w") as file_out,
         ):
             file_out.write(file_in.read())
+
     except Exception:
         return
